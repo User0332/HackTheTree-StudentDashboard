636cef5faa139a7810520ca194f81e9d89eef998
User0332 <carlzfurtado@gmail.com>
2022-06-04 22:21:23 -0400

    add backend

---
+++ backend/app.py
@@ -0,0 +1,167 @@
+from flask_session import Session
+from flask_cors import CORS
+from constants import *
+
+from flask import (
+	Flask,
+	jsonify,
+	render_template,
+	request,
+	redirect,
+	url_for,
+	session
+)
+
+from flask_socketio import (
+	SocketIO,
+	join_room,
+	leave_room,
+	emit
+)
+
+
+
+app = Flask(
+	__name__, 
+	template_folder=TEMPLATE_FOLDER,
+	static_folder=STATIC_FOLDER
+)
+
+CORS(app)
+
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_PERMANENT'] = False
+
+socketio = SocketIO(app, manage_session=False)
+
+Session(app)
+
+# FLASK METHODS
+
+@app.before_first_request
+def clear_session():
+	session.pop('username', None)
+	session.pop('room', None)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return jsonify(
+		{
+			'success': False,
+			'data': str(e)
+		}
+		), 404
+
+def login(username, room):
+	if room not in VALID_ROOMS:
+		return {
+			'success': False,
+			'data': 'Invalid room'
+		}
+
+	session['username'] = username
+	session['room'] = room
+
+	return {
+		'success': True,
+		'data': f'Successfully logged into {room} in as {username}'
+	}
+
+@app.route("/api/<api_request>", methods=['GET'])
+def api(api_request):
+	if api_request == 'username':
+		username = session.get('username', None)
+		if username is not None:
+			return jsonify(
+				{
+					'success': True,
+					'data': username
+				}
+			)
+		else:
+			return jsonify(
+				{
+					'success': False,
+					'data': 'No user logged in'
+				}
+			)
+	elif api_request == 'room':
+		room = session.get('room', None)
+		if room is not None:
+			return jsonify(
+				{
+					'success': True,
+					'data': room
+				}
+			)
+		else:
+			return jsonify(
+				{
+					'success': False,
+					'data': 'No user logged in to a room'
+				}
+			)
+	elif api_request == 'login':
+		username = request.args.get('username')
+		room = request.args.get('room')
+
+		return jsonify(
+			login(username, room)
+		)
+
+	return jsonify(
+		{
+			'success': False,
+			'data': 'Invalid request name'
+		}
+	)
+
+
+# SOCKETIO METHODS
+
+@socketio.on('send', namespace='/chat')
+def send_message(data):
+	emit(
+		'send',
+		{
+			'username': session['username'],
+			'message': data['message'] 
+		},
+		room=session['room']
+	)
+
+@socketio.on('status', namespace='/chat')
+def status(data):
+	room = session['room']
+	username = session['username']
+
+	if data['action'] == 'join':
+		join_room(room)
+		emit(
+			'status',
+			{
+				'message': f"{username} has joined the room",
+			},
+			room=room
+		)
+		return
+
+	leave_room(room)
+	session.clear()
+
+	emit(
+		'status',
+		{
+			'message': f"{username} has left the room"
+		},
+		room=room
+	)
+
+	session.pop('username', None)
+	session.pop('room', None)
+
+
+
+if __name__ == '__main__':
+	socketio.run(app, debug=True)
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	useEffect(() => {
+		fetch('http://127.0.0.1:5000/getdata', { 
+				'method': 'GET', 
+				'headers': { 
+					'Content-Type': 'application/json' 
+				} 
+			})
+			.then(response => response.json())
+			.then(data => console.log(data))
+			.catch(error => console.log(error))
+		})
+		
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	)
 }
 
 export default App;
